635936816af9aa17d9a26d281ede75722940f7a2
soyalextreme <soyalextreme@gmail.com>
2021-06-14 20:13:15 -0500

    Changes on the commit [mod] global styles for the compoent

--- styles/global.ts
+++ styles/global.ts
@@ -1,15 +1,6 @@
 import styled from "@emotion/styled";
 import { Fonts } from "../constants/Fonts";
 
-export const Body = (color: string) => {
-  const BodyPage = styled.body`
-    background-color: ${color};
-    width: 100vw;
-    height: 100vh;
-  `;
-  return BodyPage;
-};
-
 export const Link = styled.a`
   color: red;
   text-align: center;
